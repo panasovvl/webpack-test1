4b5d127e15f1277e13875a9f8261e6c76c892de7
Panasov.V.L. <panasov.v.l@gmail.com>
2025-02-06 18:22:54 +0300

    server

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
 import _ from "lodash";
-import "./style.css";
-import Icon from "./phones.png";
+import printMe from './print.js';
 
 function component() {
   const element = document.createElement("div");
+  const btn = document.createElement('button');
 
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.innerHTML = _.join(["Hello1", "webpack"], " ");
   element.classList.add("hello");
 
-  const myIcon = new Image();
-
-  myIcon.src = Icon;
-
-  element.appendChild(myIcon);
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+  element.appendChild(btn);
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,27 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.js",
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
+  mode: "development",
+  entry: {
+    index: "./src/index.js",
+    print: "./src/print.js",
   },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
-    ],
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Development",
+    }),
+  ],
+  devServer: {
+    static: "./dist",
+  },
+  devtool: "inline-source-map",
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  optimization: {
+    runtimeChunk: "single",
   },
 };
