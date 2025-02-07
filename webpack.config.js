2eecd17d39cf7a29d627f4530a084ecd3a87edcc
Panasov.V.L. <panasov.v.l@gmail.com>
2025-02-07 10:26:29 +0300

    мой вариант

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,18 @@
 const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: "./src/index.js",
   output: {
-    filename: "bundle.js",
+    filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
+    clean: true,
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
   module: {
     rules: [
       {
