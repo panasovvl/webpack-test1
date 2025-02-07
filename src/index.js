1362b0992211da7e9d206738154c46b9730ed8ba
Panasov.V.L. <panasov.v.l@gmail.com>
2025-02-07 12:21:44 +0300

    мой вариант 2

--- src/index.js
+++ src/index.js
@@ -1,18 +1,11 @@
 import _ from "lodash";
 import "./style.css";
-import Icon from "./phones.png";
 
 function component() {
   const element = document.createElement("div");
 
   element.innerHTML = _.join(["Hello", "webpack"], " ");
   element.classList.add("hello");
-
-  const myIcon = new Image();
-
-  myIcon.src = Icon;
-
-  element.appendChild(myIcon);
 
   return element;
 }
